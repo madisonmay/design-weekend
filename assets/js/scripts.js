0d073d1ce7814d0e15b904e6ef0f6fd65cdaaffd
unknown <snorthway@PC-1472.continuumnet.com>
2014-06-14 00:00:59 -0400

    pretty olin background and some new words on the first panel

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -36,9 +36,9 @@ jQuery(document).ready(function() {
         Background slideshow
     */
     $('.coming-soon').backstretch([
-      "assets/img/backgrounds/1.jpg"
-    , "assets/img/backgrounds/2.jpg"
-    , "assets/img/backgrounds/3.jpg"
+      "assets/img/backgrounds/olin_cropped.jpg"
+    // , "assets/img/backgrounds/2.jpg"
+    // , "assets/img/backgrounds/3.jpg"
     ], {duration: 3000, fade: 750});
     
     $('.about-container').backstretch("assets/img/backgrounds/2.jpg");
